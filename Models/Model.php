ae4d4b742ff41c80ce32780fdf7eb4d4b3cf19f9
berndengels <engels@f50.de>
2021-04-12 10:33:16 +0200

    stuff

--- Models/Model.php
+++ Models/Model.php
@@ -5,13 +5,9 @@ class Model extends MyDB {
     protected $table;
 
     public function all() {
-         $sql = "SELECT * FROM $this->table";
-         return $this->getAll($sql);
     }
         
     public function find(int $id) {
-        $sql = "SELECT * FROM $this->table WHERE id = ?";
-        return $this->getOne($sql, [$id]);
     }
 }
 ?>
