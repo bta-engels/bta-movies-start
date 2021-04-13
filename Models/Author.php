92daa9b3e881a50ec44004b2f8af40af12a3074c
berndengels <engels@f50.de>
2021-04-13 14:07:37 +0200

    stuff

--- Models/Author.php
+++ Models/Author.php
@@ -4,4 +4,11 @@ class Author extends Model {
 
     protected $table = 'authors';
 
+    public function find(int $id) {
+        $data = parent::find($id);        
+        // get movies
+        $sql = "SELECT * FROM movies WHERE author_id = ?";
+        $data['movies'] = $this->getAll($sql, [$id]);        return $data;
+    }
+
 }
