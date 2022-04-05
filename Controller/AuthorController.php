d75f8420ccb46155cc74f997ddf2b06202b0925a
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-05 12:13:39 +0200

    Add AuthorController and Author Model

---
+++ Controller/AuthorController.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('Controller/Controller.php');
+
+class AuthorController extends Controller {
+
+  public function index(){
+    // @todo: get authors from db (use model)
+    require_once('Views/author/index.php');
+  }
+} 
