345b5e5f2dc1f20e1c33615e2a882d937b8ccb0b
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-04-05 13:01:52 +0200

    index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'inc/Helper.php';
-
+require_once 'inc/html_header.php';
+Helper::dump($_GET);
 // initialisiere variablen
 // ID eines Datensatzes
 $id         = null;
@@ -9,6 +10,31 @@ $action     = null;
 // konstruktor eines controllers
 $controller = null;
 
+
+
+if($_GET) { 
 // @todo define controller
 // @todo define actions (controller methods) with ID or not
+if (isset($_GET['controller'])){
+switch ($_GET['controller']) {
+case 'authors':
+    require_once('Controller/AuthorController.php');
+$controler = new AuthorController;
+break;
+echo "Nichts gegeben";
+default:
+
+break;
+}
+    if ($controller && isset($_GET['action'] && method_exists($controller, $_GET['action'])) {
+    
+    //$controller->index();
+    $action = $_GET['action'];
+    $controller->$action();
+    }
+}
+} else {
+  require_once('Views/home.php');
+}
+require_once 'inc/html_footer.php';
 ?>
