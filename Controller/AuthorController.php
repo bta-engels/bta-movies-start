aaa48627cb5c72956f6a1ba226ea982cbcc3c8e1
memoray <102960499+memoray@users.noreply.github.com>
2022-04-05 15:26:34 +0200

    Änderungen Model Author AuthorController

--- Controller/AuthorController.php
+++ Controller/AuthorController.php
@@ -1,12 +1,17 @@
 <?php
 
 require_once('Controller/Controller.php');
+require_once('Models/Author.php');
 
 class AuthorController extends Controller {
 
   public function index(){
     // @todo: get authors from db (use model)
+    $model = new Author;
+    $data = $model->all();
+    Helper::dump($data);
     require_once('Views/author/index.php');
+   
   }
 
   public function show ($id){
--- Models/Model.php
+++ Models/Model.php
@@ -2,10 +2,12 @@
 require_once 'inc/MyDB.php';
 
 class Model extends MyDB {
-
+    
     protected $table;
 
     public function all() {
+        $sql = "SELECT * FROM $this->table";
+        return $this->getAll($sql);
     }
 
     public function find(int $id) {
