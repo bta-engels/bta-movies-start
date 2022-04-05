7de92f08f6b5df8eb3d678a36d9f09d7f16720bb
memoray <102960499+memoray@users.noreply.github.com>
2022-04-05 14:08:23 +0200

    Änderungen index AuthorController

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'inc/Helper.php';
+require_once('inc/html_header.php');
+
+Helper::dump($_GET);
 
 // initialisiere variablen
 // ID eines Datensatzes
@@ -9,6 +12,35 @@ $action     = null;
 // konstruktor eines controllers
 $controller = null;
 
-// @todo define controller
-// @todo define actions (controller methods) with ID or not
+
+if ($_GET) {
+    // @todo define controller
+    // @todo define actions (controller methods) with ID or not
+    //require_once('Controller/AuthorController.php');
+    //$controller = new AuthorController;
+    //$controller->index();
+
+    if (isset($_GET['controller'])){
+        switch ($_GET ['controller']){
+            case 'authors':
+                require_once('Controller/AuthorController.php');
+                $controller = new AuthorController;
+                break;
+
+            default:
+
+                break;
+        }
+    }    
+//wenn controller nicht null und gibt es parameter $action und wenn ja (method exist) gibts ne funktion in .. dann index ausgeführt
+    if ($controller && isset($_GET['action']) && method_exists($controller, $_GET ['action'])) {
+        //$controller->index();
+        $action = $_GET['action'];
+        $controller->$action();
+    }
+} else {
+    require_once('Views/home.php');
+}
+
+require_once('inc/html_footer.php');
 ?>
