0fb1e1c4d7a80b35d828b3fbb97b87f8bb1cdbc2
memoray <102960499+memoray@users.noreply.github.com>
2022-04-06 11:02:34 +0200

    In top_navigation Reiter Autoren hinzugefügt

--- inc/top_navigation.php
+++ inc/top_navigation.php
@@ -10,6 +10,9 @@
         <li class="nav-item">
             <a class="nav-link" href="/">Home</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/authors">Autoren</a>
+        </li>
     </ul>
 
     <!-- Right Side Of Navbar -->
