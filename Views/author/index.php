63a6db2ddc98f4be3d284e7c95e9658d06858e28
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-06 12:54:17 +0200

    Fix table syntax in author view

--- Views/author/index.php
+++ Views/author/index.php
@@ -2,18 +2,22 @@
 <h3>Autoren Liste</h3>
 
 <table class="table table-striped">
-   <tr>
-        <th>ID</th>
-        <th>Name</th>
-   </tr>
-   <?php foreach($data as $item): ?>
-   <tr>
-       <td><?php echo $item['id'] ?></td>
-       <td><?php
-           echo "<a href=\"/authors/$item[id]\">";
-           echo $item['firstname'] . ' ' . $item['lastname'];
-           echo '</a>';
-           ?></td>
-   </tr>
-    <?php endforeach ?>
+    <thead>
+       <tr>
+            <th>ID</th>
+            <th>Name</th>
+       </tr>
+    </thead>
+    <tbody>
+        <?php foreach($data as $item): ?>
+        <tr>
+           <td><?php echo $item['id'] ?></td>
+           <td><?php
+               echo "<a href=\"/authors/$item[id]\">";
+               echo $item['firstname'] . ' ' . $item['lastname'];
+               echo '</a>';
+               ?></td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
 </table>
