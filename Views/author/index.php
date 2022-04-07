d4c535dae1a8bdf5b2a6de4c93dd173124b4a797
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-07 12:03:32 +0200

    Add edit add & delete columns to author index

--- Views/author/index.php
+++ Views/author/index.php
@@ -6,17 +6,26 @@
        <tr>
             <th>ID</th>
             <th>Name</th>
+            <?php if ($auth) : ?>
+                <th colspan="2"></th>
+            <?php endif; ?>
        </tr>
     </thead>
     <tbody>
         <?php foreach($data as $item): ?>
         <tr>
            <td><?php echo $item['id'] ?></td>
-           <td><?php
+           <td>
+               <?php
                echo "<a href=\"/authors/$item[id]\">";
                echo $item['firstname'] . ' ' . $item['lastname'];
                echo '</a>';
-               ?></td>
+               ?>
+            </td>
+            <?php if ($auth) : ?>
+            <td>Edit</td>
+            <td>Delete</td> 
+            <?php endif; ?>           
         </tr>
         <?php endforeach ?>
     </tbody>
