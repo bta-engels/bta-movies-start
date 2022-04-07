9e3c8eb9edd96dd320007751082c1b41fe0bdc1d
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-04-07 11:37:57 +0200

    interface

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -4,7 +4,7 @@ require_once('Models/User.php');
 
 class UserController extends Controller
 {
-    public function login()
+    public function login($error = null)
     {
         require_once('Views/forms/login.php');
     }
@@ -17,13 +17,25 @@ class UserController extends Controller
             $password = $_POST['password'];
             $model = new User();
             $result = $model->check($username, $password);
-            Helper::vdump($result);
+
+            if(!$result) {
+                $this->login('Login Daten falsch!');
+            } else {
+                // login war erfolgreich
+                $_SESSION['auth'] = [
+                    'id' => $result['id'],
+                    'username'  => $result['username'],
+                ];
+                // weiterleiten auf homepage
+                header('Location: /');
+            }
         }
     }
 
     public function logout()
     {
-
+        unset($_SESSION['auth']);
+        header('Location: /');
     }
 }
 ?>
