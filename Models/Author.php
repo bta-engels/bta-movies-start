846be5eeb9e660d6eb7f172c0e9db2f9d033f26b
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-04-07 15:33:09 +0200

    bugfix

--- Models/Author.php
+++ Models/Author.php
@@ -7,12 +7,12 @@ class Author extends Model
     protected $table = 'authors';
 
     public function insert(array $params) {
-        $sql = "INSERT INTO $this->table (firstname, lastname) VALUES (:firtname, :lastname)";
+        $sql = "INSERT INTO $this->table (firstname, lastname) VALUES (:firstname, :lastname)";
         return $this->prepareAndExecute($sql, $params);
     }
 
     public function update(array $params, int $id) {
-        $sql = "UPDATE $this->table SET firstname=:firstanme, lastname=:lastname WHERE id=:id";
+        $sql = "UPDATE $this->table SET firstname=:firstname, lastname=:lastname WHERE id=:id";
         $params['id'] = $id;
         return $this->prepareAndExecute($sql, $params);
     }
