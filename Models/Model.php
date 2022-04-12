e1f3d58fd8a06f0ee87314e20e104f248cab7650
digicommons <43313518+digicommons@users.noreply.github.com>
2022-04-12 10:14:47 +0200

    Transfer insert and update functions from Author.php to Model.php

--- Models/Model.php
+++ Models/Model.php
@@ -27,9 +27,33 @@ class Model extends MyDB {
     }
 
     public function insert(array $params) {
+        $keys = array_keys($params);
+        $cols = implode(",", $keys);
+
+        function placeholder($col) {
+            return ':' . $col;
+        }
+
+        $values = implode(",", array_map('placeholder', $keys));
+
+        $sql = "INSERT INTO $this->table ($cols) VALUES ($values)";
+        return $this->prepareAndExecute($sql, $params);
     }
 
     public function update(array $params, int $id) {
+        $keys = array_keys($params);
+
+        function placeholder($col) {
+            return $col . '=:' . $col;
+        }
+
+        $values = implode(",", array_map('placeholder', $keys));
+
+        $sql = "UPDATE $this->table
+        SET $values
+        WHERE id=:id";
+        $params['id'] = $id;
+        return $this->prepareAndExecute($sql, $params);
     }
 }
 ?>
