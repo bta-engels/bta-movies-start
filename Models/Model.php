70cd95e62fc340f46ef3dd0d1b5e547474460078
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-10-24 11:14:01 +0200

    reset to project start

--- Models/Model.php
+++ Models/Model.php
@@ -1,22 +1,48 @@
 <?php
 require_once 'inc/MyDB.php';
 
+/**
+ *
+ */
 class Model extends MyDB {
 
+    /**
+     * @var
+     */
     protected $table;
 
+    /**
+     * @return void
+     */
     public function all() {
     }
 
+    /**
+     * @param int $id
+     * @return void
+     */
     public function find(int $id) {
     }
 
+    /**
+     * @param int $id
+     * @return void
+     */
     public function delete(int $id) {
     }
 
+    /**
+     * @param array $params
+     * @return void
+     */
     public function insert(array $params) {
     }
 
+    /**
+     * @param array $params
+     * @param int $id
+     * @return void
+     */
     public function update(array $params, int $id) {
     }
 }
