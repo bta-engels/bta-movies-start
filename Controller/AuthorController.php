212714326b6bec52a62a881bed101fd91295dd60
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-10-26 18:44:54 +0200

    stuff

--- Controller/AuthorController.php
+++ Controller/AuthorController.php
@@ -2,7 +2,6 @@
 require_once('Controller/IController.php');
 require_once('Controller/Controller.php');
 
-
 class AuthorController extends Controller implements IController
 {
     protected $model = Author::class;
