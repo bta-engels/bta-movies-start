851158f218407c53eb1f99c69667d6b609febc0f
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-11-02 13:05:25 +0100

    confirmation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('.delsoft').click(function(e){
+        // führe link nur aus, wenn confirmation bestätigt wurde
+        // zur absicherung des lösch-vorgangs
         return confirm('Daten wirklich löschen?');
     });
 });
