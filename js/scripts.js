1942a050f07c390e310a68b5a1d7ab3dc320a79c
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-11-02 13:13:20 +0100

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('.delsoft').click(function(e){
         // führe link nur aus, wenn confirmation bestätigt wurde
         // zur absicherung des lösch-vorgangs
+        
         return confirm('Daten wirklich löschen?');
     });
 });
